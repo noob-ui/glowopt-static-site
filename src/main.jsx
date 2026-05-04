7a0ef42beb9c9e29b64fffd0bcf3aaef1082496e
Burak <cyasin589@gmail.com>
2026-05-04 16:22:53 +0300

    Add Vercel Speedlights

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import GlowoptLandingPage from "./App.jsx";
 import "./index.css";
+
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
