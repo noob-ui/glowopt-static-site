470690a9a1e3e67df5bfe788e43330203273074b
Burak <cyasin589@gmail.com>
2026-05-04 16:26:07 +0300

    Fix Vercel analytics and speed insights for Vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import GlowoptLandingPage from "./App.jsx";
 import "./index.css";
-
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <GlowoptLandingPage />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
